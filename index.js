4a428d3f4349d7105fffc6ec11cbebb8859036af
Marcos Abre <marcosabre15@gmail.com>
2021-10-14 19:52:46 -0300

    removiendo carpetas

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require ("express");
+const app = express();
+const bodyParser = require ("body-parser");
+const cors = require ("cors");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(
+	process.env.DB_CONNECTION,
+	{
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	}
+), (req,res) => {
+	console.log("conectado a la base de datos");
+}
+
+const localesRoutes = require("./api/Routes/Locales/local");
+
+
+app.use("/api/locales/", localesRoutes);
+
+
+app.listen(PORT, () =>{
+	console.log("escuchando puerto 5000")
+})
+
